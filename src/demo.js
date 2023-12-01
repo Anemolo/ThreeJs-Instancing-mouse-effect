ae9f00537ab852ed193c2b443a3ae09fb1aa6cc1
Daniel Velasquez <velasquezdanield@gmail.com>
2023-11-30 22:48:05 -0500

    .

--- src/demo.js
+++ src/demo.js
@@ -4,9 +4,11 @@ import { gsap } from "gsap"
 import { Rendering } from "./rendering"
 
 import * as THREE from "three";
+import RoundedBox from './RoundedBox'
 
 class InstancedMouseEffect {
-  constructor(opts = {} ){
+  constructor(opts = {}, follower = null ){
+
 
     if(opts.speed == null) {
       opts.speed = 1
@@ -20,8 +22,20 @@ class InstancedMouseEffect {
     if(opts.rotationSpeed == null) {
       opts.rotationSpeed = 1
     }
+    if(opts.rotationAmmount == null) {
+      opts.rotationAmmount = 0
+    }
+    if(opts.mouseScaling == null) {
+      opts.mouseScaling = 0
+    }
+    if(opts.mouseIndent == null) {
+      opts.mouseIndent = 1
+    }
     if(opts.color == null) {
       opts.color = "#1084ff"
+    }
+    if(opts.colorDegrade == null) {
+      opts.colorDegrade = 1.
     }
     if(opts.shape == null) {
       opts.shape = "square"
@@ -35,6 +49,19 @@ class InstancedMouseEffect {
     rendering.camera.position.x = 40
     rendering.camera.lookAt(new THREE.Vector3(0,0,0))
     this.rendering = rendering;
+
+
+    //  follower = new THREE.Mesh(
+    //    new THREE.BoxGeometry(),
+    //    new THREE.MeshPhysicalMaterial({ 
+    //      emissive: 0x000000, 
+    //      color:  "#ff4080", 
+    //      color:  "#2040bb", 
+    //      roughness: 0,
+    //      metalness: 0.4
+    //    })
+    //  )
+    // rendering.scene.add(follower)
 
     // let controls = new OrbitControls(rendering.camera, rendering.canvas)
 
@@ -70,6 +97,7 @@ class InstancedMouseEffect {
     let gridSize = grid * size
 
     let geometry = new THREE.BoxGeometry(size, size, size);
+    geometry = new RoundedBox(size, size, size, 0.1, 4);
     if(typeof(opts.shape) == "string"){
       switch(opts.shape){
         case "cylinder":
@@ -120,7 +148,7 @@ class InstancedMouseEffect {
       dummy.updateMatrix()
       mesh.setMatrixAt(i, dummy.matrix)
 
-      let center = 1.- dummy.position.length() * 0.18
+      let center = 1.- dummy.position.length() * 0.12 * opts.colorDegrade
       color.set( center * weights.x + (1.-weights.x) , center * weights.y + (1.-weights.y) , center * weights.z + (1.-weights.z))
       mesh.setColorAt(i,color)
 
@@ -133,6 +161,7 @@ class InstancedMouseEffect {
       uniform vec2 uPos0;
       uniform vec2 uPos1;
       uniform vec4 uConfig;
+      uniform vec4 uConfig2;
 
       float map(float value, float min1, float max1, float min2, float max2) {
         return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
@@ -171,10 +200,10 @@ class InstancedMouseEffect {
 
             // float mouseTrail = length(position.xz- uPos0.xy);
             float mouseTrail = sdSegment(position.xz, uPos0, uPos1 );
-            mouseTrail = smoothstep(1., 3. * uConfig.z , mouseTrail)  ;
+            mouseTrail = smoothstep(2.0, 5. * uConfig.z , mouseTrail)  ;
 
             // Mouse Scale
-            transformed *= 1. + cubicOut(1.0-mouseTrail) * 2.;
+            transformed *= 1. + cubicOut(1.0-mouseTrail) * uConfig2.y;
 
 
             // Instance Animation
@@ -183,15 +212,15 @@ class InstancedMouseEffect {
             float anim = (map(clamp(uAnimate, start,end) , start, end, 0., 1.));
 
 
-            transformed = rotate(transformed, vec3(0., 1., 1. ),anim * 3.14+ mouseTrail * 3.14 +  uTime * uConfig.x + toCenter * 0.4 * uConfig.w );
+            transformed = rotate(transformed, vec3(0., 1., 1. ),uConfig2.x * (anim * 3.14+  uTime * uConfig.x + toCenter * 0.4 * uConfig.w) );
 
             // Mouse Offset
-            transformed.y += -2.9 * (1.-mouseTrail);
+            transformed.y += (-1.0 * (1.-mouseTrail)) * uConfig2.z;
 
             transformed.xyz *= cubicInOut(anim);
             transformed.y += cubicInOut(1.-anim) * 1.;
 
-            transformed.y += sin(uTime * 2. * uConfig.x + toCenter * uConfig.y) * 0.3;
+            transformed.y += sin(uTime * 2. * uConfig.x + toCenter * uConfig.y) * 0.1;
 
             vec4 mvPosition = vec4( transformed, 1.0 );
 
@@ -210,7 +239,8 @@ class InstancedMouseEffect {
       uPos0: {value: new THREE.Vector2()},
       uPos1: {value: new THREE.Vector2()},
       uAnimate: {value: 0},
-      uConfig: { value: new THREE.Vector4(opts.speed, opts.frequency, opts.mouseSize, opts.rotationSpeed)}
+      uConfig: { value: new THREE.Vector4(opts.speed, opts.frequency, opts.mouseSize, opts.rotationSpeed)},
+      uConfig2: { value: new THREE.Vector4(opts.rotationAmmount, opts.mouseScaling, opts.mouseIndent)}
     }
     mesh.material.onBeforeCompile = (shader)=>{
       shader.vertexShader = shader.vertexShader.replace("void main() {", vertexHead)
@@ -241,6 +271,14 @@ class InstancedMouseEffect {
       duration: 3.0,
       ease: "none"
     }, 0.0)
+    if(follower){
+
+    t1.from(follower.scale, 
+        {x: 0, y: 0, z: 0, duration: 1, ease: "back.out"},
+        1,
+      )
+    }
+    this.animation = t1;
 
     // Events
     const hitplane = new THREE.Mesh(
@@ -275,7 +313,7 @@ class InstancedMouseEffect {
     })
 
     let vel = new THREE.Vector2()
-    const tick = (t)=>{
+    const tick = (t, delta)=>{
       uTime.value = t 
 
       let v3 = new THREE.Vector2()
@@ -297,8 +335,15 @@ class InstancedMouseEffect {
       // Add the lerped velocity
       uniforms.uPos1.value.add(vel)
 
+      if(follower ){
+        follower.position.x = uniforms.uPos0.value.x
+        follower.position.z = uniforms.uPos0.value.y
+        follower.rotation.x = t 
+        follower.rotation.y = t 
+      }
 
       rendering.render()
+
     }
 
     gsap.ticker.add(tick)
